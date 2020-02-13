3f79998f51f349d5c92008b9c1d2756023fcbb2f
tyson boren <tyson.d.boren@gmail.com>
2020-02-13 09:10:17 -0700

    pushed for and while

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,2 +1,42 @@
-// ES6 code goes here
-console.log('es6 code goes here');
+// - Create a new es6-starter project
+// - Call that project: js-code-challenges
+// - Change into that folder
+// - Open that folder into vscode
+// - Install your dependencies
+// - Start your server
+// - Check to make sure your project is working in the browser
+// - Initialize git for that folder
+// - Create a .gitignore file and ignore your node_modules
+// - Create a github repo
+// - Push your code up to github
+// - Inside your bootstrap.js file create three functions -START FROM HERE WHEN YOU START UP AGAIN. 
+// - Write three functions that compute the sum of the numbers in an array: using a for-loop, a while loop, do-while loop.
+// - Push your code up to github
+
+function myFunc() {
+  var numbers = [2, 3]
+  for (num in numbers) {
+    return numbers[0] ** numbers[1]
+  }  
+  
+}
+
+
+console.log(myFunc())
+
+
+function loopAWhile(array) {
+  var sum = 0
+  var i = 0
+   while (i < array.length) {
+    sum += array[i];
+    i++;
+  }
+  console.log(sum) 
+}
+
+loopAWhile([1,2,3])
+
+
+
+
